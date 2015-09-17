5ca44096cb467814f8b5c6f149af52392f84538b
Josh Wu <josh.wu@atmel.com>
2015-09-17 17:49:30 +0800

    scripts: gen_ubootenv.sh: give a board name, it will generate an uboot env
    Take at91sam9n12ek as an example:
    
      env_common.txt
      env_nand.txt
           \
           |---- at91sam9n12ek/env_common.txt
           |---- at91sam9n12ek/env_nand.txt
    
    1. Script will combine env_common.txt and env_nand.txt together to
       env.txt
    2. Also it will combine at91sam9n12ek folders env_common.txt and
       env_nand.txt to env.txt
    3. Use variables of at91sam9n12ek/env.txt to replace the variables in
       env.txt. And finally generate a file env_tmp.txt.
    4. Generate a u-boot env binary from env_tmp.txt by call mkenvimage.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>

---
+++ scripts/gen_ubootenv.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Usage: ./gen_ubootenv.sh <board name>
+
+# $1: board folder, which includes boot.bin, u-boot.bin
+# create a env_tmp.txt for build, which will override the board
+# specified U-Boot variables.
+function combine_uboot_env() {
+	local _BOARD_PATH=$1
+
+	cp env.txt env_tmp.txt
+	if [ ! -f ${_BOARD_PATH}/env.txt ]; then
+		# just use save env.txt
+		return 0;
+	fi;
+
+	# find variables in board path.
+	local _VAR_NAMES=$(awk -F= '{print $1}' ${_BOARD_PATH}/env.txt)
+	if [ "x${_VAR_NAMES}" = "x" ]; then
+		echo "Error: not found the variable in: ${_BOARD_PATH}/env.txt"
+		return 1
+	fi
+
+	# delete the overrided variables on env_tmp.txt
+	for _VAR_NAME in ${_VAR_NAMES}
+	do
+		sed -i "/^${_VAR_NAME}=/ d" env_tmp.txt
+	done
+
+	# combine the board's env.txt to the end of env_tmp.txt
+	cat ${_BOARD_PATH}/env.txt >> env_tmp.txt
+
+	return 0
+}
+
+function generate_random_eth_addr() {
+	local _VAR_UBOOT_ETH="ethaddr="
+	local _VAR_UBOOT_ETH1="eth1addr="
+
+	local _VAR_ADDR=`./scripts/gen_ethaddr.sh`
+	local _VAR_ADDR1=`./scripts/gen_ethaddr.sh`
+
+	echo "${_VAR_UBOOT_ETH}${_VAR_ADDR}" >> env.txt
+	echo "${_VAR_UBOOT_ETH1}${_VAR_ADDR1}" >> env.txt
+}
+
+function prepare_uboot_env() {
+	local _BOARD_PATH=$1
+
+	# combile env_nand.txt to env.txt
+	cat env_common.txt env_nand.txt > env.txt
+
+	cat ${_BOARD_PATH}/env_common.txt ${_BOARD_PATH}/env_nand.txt > ${_BOARD_PATH}/env.txt
+}
+
+# check parameters
+if [ $# != 1 ]; then
+	echo "Error: should specify a board name as a parameter!"
+	exit 1
+fi
+
+# choose the which board to boot
+BOARD_DIR=$(find . -maxdepth 1 -type d -name "*$1*")
+if [ "x${BOARD_DIR}" = "x" ]; then
+	echo "Error: not found the board folder for: $1"
+	exit 1
+fi
+
+# TODO: if we find more than one board folder?
+
+# generate env.txt
+prepare_uboot_env ${BOARD_DIR}
+
+# generate 2 eth addresses append to env.txt
+generate_random_eth_addr
+
+# combine the env.txt file for spcified board
+combine_uboot_env ${BOARD_DIR}
+if [ $? -ne 0 ]; then
+	echo "Error: cannot combile the board env.txt!"
+	exit 1
+fi
+
+# generate a uboot.env from env_tmp.txt.
+# -s CONFIG_ENV_SIZE
+
+mkdir -p output/sd
+mkdir -p output/spi
+
+# sd card env file
+./mkenvimage -s 0x4000 -o output/sd/uboot.env env_tmp.txt
+
+# spi env binary
+./mkenvimage -s 0x4000 -p 0x0 -o output/spi/uboot.env env_tmp.txt
+
